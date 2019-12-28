d9a5d99bdd01159112a82bb8883a25ef167458aa
HytonightYX <whosiyuan@qq.com>
2019-12-28 19:13:00 +0800

    CleanWebpackPlugin build先清除残留打包文件

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
 	mode: 'development', // 开发模式
@@ -13,7 +14,15 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template:path.resolve(__dirname,'../public/index.html')
-		})
+			template: path.resolve(__dirname, '../public/index.html'),
+			filename: 'index.html',
+			chunks: ['main'] // 与入口文件对应的模块名
+		}),
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, '../public/header.html'),
+			filename: 'header.html',
+			chunks: ['header'] // 与入口文件对应的模块名
+		}),
+		new CleanWebpackPlugin()
 	]
 }
