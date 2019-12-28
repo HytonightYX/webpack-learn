8f3f7c5066e9a96fc5bb642b77f378cfa27c98d6
HytonightYX <whosiyuan@qq.com>
2019-12-28 19:12:42 +0800

    多文件入口

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,15 +1,17 @@
-// webpack.config.js
-const path = require('path');
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-	mode:'development', // 开发模式
-	entry: path.resolve(__dirname,'../src/main.js'),    // 入口文件
+	mode: 'development', // 开发模式
+	entry: {
+		main: path.resolve(__dirname, '../src/main.js'),
+		header: path.resolve(__dirname, '../src/header.js')
+	},
 	output: {
 		filename: '[name].[hash:8].js',      // 打包后的文件名称
-		path: path.resolve(__dirname,'../dist')  // 打包后的目录
+		path: path.resolve(__dirname, '../dist')  // 打包后的目录
 	},
-	plugins:[
+	plugins: [
 		new HtmlWebpackPlugin({
 			template:path.resolve(__dirname,'../public/index.html')
 		})
